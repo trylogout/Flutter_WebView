67672575671b0a8d071e2f05786261654003f6e5
trylogout <kurtswrc@gmail.com>
2020-09-04 14:27:40 +0500

    GoBack() and UrlCheck

---
+++ lib/screens/web_view_container.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebViewContainer extends StatefulWidget {
+  final url;
+
+  WebViewContainer(this.url);
+
+  @override
+  createState() => _WebViewContainerState(this.url);
+}
+
+WebViewController controllerGlobal;
+
+Future<bool> _exitApp(BuildContext context) async {
+  if (await controllerGlobal.canGoBack()) {
+    print("onwill goback");
+    controllerGlobal.goBack();
+  } else {
+    Scaffold.of(context).showSnackBar(
+      const SnackBar(content: Text("No back history item")),
+    );
+    return Future.value(false);
+  }
+}
+
+class _WebViewContainerState extends State<WebViewContainer> {
+  var _url;
+  final _key = UniqueKey();
+
+  _WebViewContainerState(this._url);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(),
+        body: Column(
+          children: [
+            Expanded(
+                child: WebView(
+                    key: _key,
+                    javascriptMode: JavascriptMode.unrestricted,
+                    initialUrl: _url))
+          ],
+        ));
+  }
+}
